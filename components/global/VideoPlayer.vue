41223eb8b60c918d76ee70b12f6d94b6ae1cb288
HamidNE <hamidne@mail.ru>
2020-01-18 00:28:44 +0330

    edit for video player page

---
+++ components/global/VideoPlayer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <video ref="videoPlayer" class="video-js"></video>
+  </div>
+</template>
+
+<script>
+import videojs from "video.js";
+
+export default {
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {
+      player: null
+    };
+  },
+  mounted() {
+    this.player = videojs(
+      this.$refs.videoPlayer,
+      this.options,
+      function onPlayerReady() {
+        console.log("onPlayerReady", this);
+      }
+    );
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+};
+</script>
+
+<style src="video.js/dist/video-js.css"></style>
--- pages/videos/_id.vue
+++ pages/videos/_id.vue
@@ -1,40 +1,27 @@
 <template>
   <b-container>
-    <link
-      href="https://unpkg.com/video.js/dist/video-js.css"
-      rel="stylesheet"
-    />
-    <script src="https://unpkg.com/video.js/dist/video.js"></script>
-    <script src="https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script>
     <h1 v-text="item.stream_path"></h1>
-    <video
-      id="my_video_1"
-      class="video-js vjs-default-skin"
-      controls
-      preload="auto"
-      width="640"
-      height="268"
-      data-setup="{}"
-    >
-      <source :src="item.stream_path" type="application/x-mpegURL" />
-    </video>
-    <!-- <script src="https://unpkg.com/videojs-flash/dist/videojs-flash.js"></script>
-    <script src="https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script> -->
-    <!-- <script src="//vjs.zencdn.net/7.3.0/video.min.js"></script>
-    <script src="https://unpkg.com/browse/@videojs/http-streaming@1.11.2/dist/videojs-http-streaming.min.js"></script> -->
+    <video-player :options="videoOptions"/>
   </b-container>
 </template>
 
 <script>
 import Video from "~/models/Video";
-import videojs from "videojs-contrib-hls";
+import VideoPlayer from "~/components/global/VideoPlayer";
 export default {
-  data: () => ({
-    form: {}
-  }),
-  mounted() {
-    // const player = videojs("vid1");
-    // player.play();
+  components: { VideoPlayer },
+  data() {
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        sources: [
+          {
+            src: "https://d2zihajmogu5jn.cloudfront.net/sintel/master.m3u8",
+          }
+        ]
+      }
+    };
   },
   async asyncData({ params }) {
     return { item: await Video.$find(params.id) };
